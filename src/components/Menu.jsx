73a9a9f72a52f31bed00f90e84f67adc1bf7c38a
Wathil <Yannick.garcia@icloud.com>
2022-01-09 21:43:13 +0100

    Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,76 @@
+import "./menu.css";
+import React, { useState, useEffect, useRef, createRef } from "react";
+
+const items = [
+  {
+    name: "Pano225",
+    color: "#f44336",
+    href: "#1",
+  },
+  {
+    name: "Artiste",
+    color: "#e91e63",
+    href: "#2",
+  },
+  {
+    name: "Zone",
+    color: "#9c27b0",
+    href: "#3",
+  },
+  {
+    name: "Musique",
+    color: "#673ab7",
+    href: "#4",
+  },
+];
+
+const MenuItem = ({ items }) => {
+  const $root = useRef();
+  const $indicator = useRef();
+  const $items = useRef(items.map(createRef));
+  const [active, setActive] = useState(0); // index active
+  const [previous, setPrevious] = useState(0); // index previous
+
+  const animate = () => {
+    const menuOffset = $root.current.getBoundingClientRect();
+    const { width, height, top, left } =
+      $items.current[active].current.getBoundingClientRect();
+
+    $indicator.current.style.left = left - menuOffset.x + "px";
+    $indicator.current.style.top = top - menuOffset.y + "px";
+    $indicator.current.style.width = width + "px";
+    $indicator.current.style.height = height + "px";
+
+    $indicator.current.style.backgroundColor = items[active].color;
+    setPrevious(active);
+  };
+
+  useEffect(() => {
+    animate();
+  }, [active]);
+
+  return (
+    <div ref={$root} className="menu">
+      {items.map((item, index) => (
+        <a
+          key={item.name}
+          ref={$items.current[index]}
+          className={`item ${active === index ? "active" : ""}`}
+          onClick={() => {
+            setActive(index);
+          }}
+          href={item.href}
+        >
+          {item.name}
+        </a>
+      ))}
+      <div ref={$indicator} className="indicator" />
+    </div>
+  );
+};
+
+const Menu = () => {
+  return <MenuItem items={items} />;
+};
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Menu from './components/Menu';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Menu />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
