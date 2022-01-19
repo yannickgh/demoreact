89751ae4e4d935b3ea1bda3945cc5e4b52e5ea81
yannickgh <yannickgh@icloud.com>
2022-01-19 13:48:54 +0100

    fix menu

--- src/getUrl.js
+++ src/getUrl.js
@@ -1,5 +1,5 @@
-const URL_START = 'https://warm-taiga-19057.herokuapp.com';
-// const URL_START = "http://localhost:8080";
+// const URL_START = 'https://warm-taiga-19057.herokuapp.com';
+const URL_START = "http://localhost:8080";
 
 export const getURLStart = () => {
   return URL_START;
